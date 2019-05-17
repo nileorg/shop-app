9d61fcf41d1d6254e255d1e2082b8ed5f094d6dc
huhn <sehe89@gmail.com>
2019-05-17 12:57:52 +0200

    add element ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import store from './store'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
