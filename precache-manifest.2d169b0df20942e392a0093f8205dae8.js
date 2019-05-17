a14b0438ae530485944f25e1d31f8d9fb9939601
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 11:41:32 +0000

    Deploy nileorg/shop-app to github.com/nileorg/shop-app.git:gh-pages

---
+++ precache-manifest.2d169b0df20942e392a0093f8205dae8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a543c5dbbc2ccd6e1a13",
+    "url": "/shop-app/js/about.c0a846dd.js"
+  },
+  {
+    "revision": "1ee8243577b97a8e1faf",
+    "url": "/shop-app/css/app.6622558b.css"
+  },
+  {
+    "revision": "1ee8243577b97a8e1faf",
+    "url": "/shop-app/js/app.43ccc43e.js"
+  },
+  {
+    "revision": "e65304102ba3226a1ffb",
+    "url": "/shop-app/js/chunk-vendors.fd66d823.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/shop-app/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/shop-app/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/shop-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9193b89ff856bf43f319e16d9422ff80",
+    "url": "/shop-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shop-app/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/shop-app/precache-manifest.2d169b0df20942e392a0093f8205dae8.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "shop-app"});
+
+workbox.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
